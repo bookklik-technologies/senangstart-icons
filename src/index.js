309e0e03054d6b01a109deb6a4fe7d6da1c555e4
A.Hakim Noor <a.hakim.solution@gmail.com>
2025-11-25 17:43:52 +0800

    feat: Implement Webpack for project bundling with Babel and CSS processing, and add build scripts.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Main entry point for webpack bundle
+import { icons } from "./icons.js";
+import "./ss-icon.js";
+import "./ss-loader.js";
+import "./style.css";
+
+// Export icons for library usage
+export { icons };
+export default { icons };
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    "senangstart-icon": "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].min.js",
+    library: {
+      name: "SenangStartIcons",
+      type: "umd",
+      export: "default",
+    },
+    globalObject: "this",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "senangstart.min.css",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  devtool: "source-map",
+};
